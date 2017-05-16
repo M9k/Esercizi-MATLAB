564621bc550585aacc14dd60f6094c11e3b55d55
Mirco <mcailotto96@gmail.com>
2017-05-16 10:18:08 +0200

    fix

--- triinfscript.m
+++ triinfscript.m
@@ -1,6 +1,6 @@
 clear all;
 
-L = [3.5 , 0   , 0;
+L = [3.5  , 0   , 0;
      4.04 , 26.4, 0;
      6.08 , 37.8, 100.6];
 b = [2.46; 11.55; 31.1];
@@ -8,17 +8,17 @@ b = [2.46; 11.55; 31.1];
 n = size(b,1);
 disp(['dimensione: ', num2str(n)]);
 
-if all(all(L - tril(L))) == 0
+if any(any(L - tril(L))) == 0
     disp('matrice diagonale inferiore');
+    
+    [xv, flag] = triinf(L, b);
+
+    disp('Matrice L:');
+    disp(L);
+    disp('Vettore b:');
+    disp(b);
+    disp('Soluzione:');
+    disp(xv);
 else
     disp('matrice non valida');
 end
-
-[xv, flag] = triinf(L, b);
-
-disp('Matrice L:');
-disp(L);
-disp('Vettore b:');
-disp(b);
-disp('Soluzione:');
-disp(xv);
--- trisupscript.m
+++ trisupscript.m
@@ -8,17 +8,17 @@ b = [4.92; 23.1; 62.2];
 n = size(b,1);
 disp(['dimensione: ', num2str(n)]);
 
-if all(all(U - triu(U))) == 0
+if any(any(U - triu(U))) == 0
     disp('matrice diagonale superiore');
+    
+    [xv, flag] = trisup(U, b);
+
+    disp('Matrice U:');
+    disp(U);
+    disp('Vettore b:');
+    disp(b);
+    disp('Soluzione:');
+    disp(xv);
 else
     disp('matrice non valida');
 end
-
-[xv, flag] = trisup(U, b);
-
-disp('Matrice U:');
-disp(U);
-disp('Vettore b:');
-disp(b);
-disp('Soluzione:');
-disp(xv);
