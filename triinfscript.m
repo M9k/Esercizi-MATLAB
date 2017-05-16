dd464b7edd0fb9cfea906e095124c63d09f6e820
Mirco <mcailotto96@gmail.com>
2017-05-16 10:41:15 +0200

    Add files via upload

--- triinfscript.m
+++ triinfscript.m
@@ -5,12 +5,12 @@ L = [3.5  , 0   , 0;
      6.08 , 37.8, 100.6];
 b = [2.46; 11.55; 31.1];
 
-n = size(b,1);
-disp(['dimensione: ', num2str(n)]);
-
 if (any(any(L - tril(L))) == 0) && (size(L,1) == size(L,2)) && (size(L,1) == size(b,1))
     disp('matrice diagonale inferiore');
     
+    n = size(b,1);
+    disp(['dimensione: ', num2str(n)]);
+
     [xv, flag] = triinf(L, b);
 
     disp('Matrice L:');
--- trisupscript.m
+++ trisupscript.m
@@ -5,12 +5,12 @@ U = [1.75, 2.02, 3.04;
      0   , 0   , 50.3];
 b = [4.92; 23.1; 62.2];
 
-n = size(b,1);
-disp(['dimensione: ', num2str(n)]);
-
 if any(any(U - triu(U))) == 0 && (size(U,1) == size(U,2)) && (size(U,1) == size(b,1))
     disp('matrice diagonale superiore');
     
+    n = size(b,1);
+    disp(['dimensione: ', num2str(n)]);
+
     [xv, flag] = trisup(U, b);
 
     disp('Matrice U:');
